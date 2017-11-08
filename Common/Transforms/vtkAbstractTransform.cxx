ed44a60049072d5bcfbf6e3bc59917499f0d3910
Julien Finet <julien.finet@kitware.com>
2017-11-08 18:34:05 +0100

    Fix crash in transforms when doing DeepCopy after Identity

--- Common/Transforms/vtkAbstractTransform.cxx
+++ Common/Transforms/vtkAbstractTransform.cxx
@@ -653,10 +653,12 @@ void vtkTransformConcatenation::Identity()
       if (tuple->ForwardTransform)
       {
         tuple->ForwardTransform->Delete();
+        tuple->ForwardTransform = nullptr;
       }
       if (tuple->InverseTransform)
       {
         tuple->InverseTransform->Delete();
+        tuple->InverseTransform = nullptr;
       }
     }
   }
